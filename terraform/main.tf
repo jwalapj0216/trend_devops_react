8c590da100aa7887e0bacbf69297ecd9e0b831fe
jwalapj0216 <jwalapj02@gmail.com>
2026-04-14 08:43:42 +0530

    Update EC2 key name to 'aws-new-dev'

--- terraform/main.tf
+++ terraform/main.tf
@@ -112,7 +112,7 @@ resource "aws_instance" "jenkins_server" {
 
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
-  key_name = "new"
+  key_name = "aws-new-dev"
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
